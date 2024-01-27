7de7ab702a374e9804154079475d75ced7cb71c8
Ema0502 <ema050296@gmail.com>
2024-01-27 09:58:41 -0300

    test function + corrections id

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,8 +1,20 @@
-import { IDiaryEntry } from "../types/types";
+import { IDiaryEntry, NonSensitiveDiaryEntry } from "../types/types";
 import diaryData from "../utils/api-utils.json";
 
 const diaries: Array<IDiaryEntry> = diaryData as Array<IDiaryEntry>;
 
-export const getEntries = () => diaries;
+export const getEntries = (): IDiaryEntry[] => diaries;
+
+// ts no es capaz de filtrar el tipo de resultado de la función que se le pasa a filtrar
+// solo controla la informacion estatica
+export const getWithoutSensitiveDiaryInfo = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({id, type, dates}) => {
+    return {
+      id,
+      type,
+      dates
+    }
+  });
+}; 
 
 export const addEntry = () => null;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,12 @@
 export type dates = "01-01-2020" | "02-01-2020" | "03-01-2020" | "04-01-2020" | "05-01-2020" | "06-01-2020" | "07-01-2020" | "08-01-2020" | "09-01-2020" | "10-01-2020"
 
 export interface IDiaryEntry {
-  "id uuid": string
+  "id": string
   "type": string
   "dates": dates
   "commentary": string
 }
+// export type NonSensitiveDiaryEntry = Pick<IDiaryEntry, "id uuid" | "type" | "dates">;
+// export type NonSensitiveDiaryEntry = Pick<IDiaryEntry,  Exclude<keyof IDiaryEntry, 'commentary'>>;
+
+export type NonSensitiveDiaryEntry = Omit<IDiaryEntry, "commentary">
